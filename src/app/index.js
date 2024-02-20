b996e7dc13ed46701bdf0872d84641f95af1d550
wczy-ao <wanzhi886@163.com>
2024-02-20 13:58:44 +0800

    动态加载路由

--- src/app/index.js
+++ src/app/index.js
@@ -3,15 +3,13 @@ const userRouter = require("../router/user.router");
 const authRouter = require("../router/auth.router");
 const errorHandle = require('./errorHandle')
 const { bodyParser } = require("@koa/bodyparser");
+const useRouters = require('../router/index')
+
+
 const app = new Koa();
 
 app.use(bodyParser());
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
-
-app.use(authRouter.routes());
-app.use(authRouter.allowedMethods());
-
+useRouters(app)
 app.on("error", errorHandle);
 
 module.exports = app;
